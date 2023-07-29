3558eae8cc6ba06bcb1941fe8cdec062f0c95983
joaopedrobrgs <joaopedrobrgsantos@gmail.com>
2023-07-29 17:21:08 -0300

    adicionando StyleSheet ao componente Home

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,12 +1,19 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 interface Props {}
 
 export default function Home({}: Props) {
   return (
-    <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Olá Dev!</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create(
+  {
+  container: {flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "#121214"},
+  title: {fontSize: 20, fontWeight: "bold", color: "white"}
+  }
+)
