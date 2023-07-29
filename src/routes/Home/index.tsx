dec9291a9ef096dc472e10d061ce70d589558e43
joaopedrobrgs <joaopedrobrgsantos@gmail.com>
2023-07-29 17:40:32 -0300

    additions: safeAreaView; fix: some styles of components of screenHome

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,19 +1,32 @@
-import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import React from 'react';
+import {Text, View, StyleSheet, SafeAreaView} from 'react-native';
 
 interface Props {}
 
 export default function Home({}: Props) {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Olá Dev!</Text>
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Welcome, Dev!</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create(
-  {
-  container: {flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "#121214"},
-  title: {fontSize: 20, fontWeight: "bold", color: "white"}
-  }
-)
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#121214',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#121214',
+    paddingHorizontal: 30,
+    paddingVertical: 50
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
