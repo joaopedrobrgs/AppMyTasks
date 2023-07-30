a4f992e40f63fb25fe04ef231adef885c7195d1f
joaopedrobrgs <joaopedrobrgsantos@gmail.com>
2023-07-30 07:20:45 -0300

    criado um estado React para armazenar novas tarefas

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,18 +1,36 @@
-import React from 'react';
-import {Text, TextInput, View, StyleSheet, SafeAreaView, Platform, TouchableOpacity} from 'react-native';
+import React, {useState} from 'react';
+import {
+  Text,
+  TextInput,
+  View,
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  TouchableOpacity,
+} from 'react-native';
 
 interface Props {}
 
 export default function Home({}: Props) {
+  const [newTask, setNewTask] = useState("");
+
   return (
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.container}>
         <Text style={styles.title}>Welcome, Dev!</Text>
-        <TextInput style={styles.input} placeholder='Nova tarefa...' placeholderTextColor='#555'/>
+        <TextInput
+          style={styles.input}
+          placeholder="Nova tarefa..."
+          placeholderTextColor="#555"
+          value={newTask || ""}
+          // onChange={(e:any):void => {setNewTask(e.target.value)}}
+          onChangeText={setNewTask}
+        />
         <TouchableOpacity activeOpacity={0.7} style={styles.button}>
           <Text style={styles.buttonText}>Adicionar</Text>
         </TouchableOpacity>
         <Text style={styles.titleDesks}>Minhas tarefas</Text>
+        <Text style={{color: "white"}}>{newTask}</Text>
       </View>
     </SafeAreaView>
   );
@@ -27,7 +45,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#121214',
     paddingHorizontal: 30,
-    paddingVertical: 50
+    paddingVertical: 50,
   },
   title: {
     fontSize: 24,
@@ -38,26 +56,26 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
     color: 'white',
-    marginVertical: 30
+    marginVertical: 30,
   },
   input: {
-    backgroundColor: "#29292e",
-    color: "#f1f1f1",
+    backgroundColor: '#29292e',
+    color: '#f1f1f1',
     fontSize: 18,
     padding: Platform.OS === 'ios' ? 15 : 12,
     marginTop: 30,
-    borderRadius: 7
+    borderRadius: 7,
   },
   button: {
-    backgroundColor: "#eba417",
+    backgroundColor: '#eba417',
     padding: Platform.OS === 'ios' ? 15 : 12,
     borderRadius: 7,
     alignItems: 'center',
-    marginTop: 20
+    marginTop: 20,
   },
   buttonText: {
     color: '#121214',
     fontSize: 18,
     fontWeight: 'bold',
-  }
+  },
 });
